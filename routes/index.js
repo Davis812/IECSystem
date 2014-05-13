e64fb0409a1d7ebd393377a5e37fa93a328cbd68
Davis0812 <shaodawei0812@mail.com>
2014-05-13 10:37:10 +0800

    140513

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@
 module.exports = function(app){
   app.get('/',function(req,res){
   res.render('login', { title: '登录' });
- });
+ });//end app.get('/')
   
   app.get('/home',function(req,res){
 	res.render('home', { title: '主页' });
-	});
-};//end function(app)
+	});//end app.get('/home')
+};//end function(app){};
