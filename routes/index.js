fcbb63fae8d8399c29945a1be66546ae23893bb2
Davis0812 <shaodawei0812@gmail.com>
2014-07-20 15:51:12 +0800

    edit

--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,6 @@ var Setpoint = require('../models/getsetpoint.js');
 var Complain = require('../models/complain.js');
 
 module.exports = function(app) {
-//	function checkNotLogin(req, res, next) {
-//		if (req.session.user) {
-//			req.flash('error', '已登入');
-//			return res.redirect('/home');
-//		}
-//		next();
-//	}
-//	
-//	function checkLogin(req, res, next) {
-//		if (!req.session.user) {
-//			req.flash('error', '未登入');
-//			return res.redirect('/');
-//		}
-//		next();
-//	}
-	
-	//app.get('/', checkNotLogin);
 	app.get('/', function(req, res) {
 		res.render('login', {
 			title : '登录',
@@ -34,11 +17,8 @@ module.exports = function(app) {
 			error: req.flash('error').toString()
 		});
 	});
-
     //app.post('/', checkNotLogin);
 	app.post('/', function(req, res) {
-		console.log(req.body.username);
-		console.log(req.body.password);
 		var password = req.body.password;
 		User.get(req.body.username, function(err, user) {
 			if (!user) {
@@ -162,5 +142,5 @@ module.exports = function(app) {
 			req.flash('退出系统').toString();
 			res.redirect('/');
 		});
-};// end function(app){};
+};
 
